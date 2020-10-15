5c8e701880bcfaf7be63ce1699263b944c6d1fbb
CCF-ChanLab <72822072+CCF-ChanLab@users.noreply.github.com>
2020-10-14 23:13:12 -0400

    Update GridSearch.py

--- Code/GridSearch.py
+++ Code/GridSearch.py
@@ -15,20 +15,20 @@ y_train = pd.DataFrame(data, columns=["Response"])
 rf16=["Cancer_Type2", "Albumin", "HED", "TMB", "FCNA", "BMI", "NLR", "Platelets", "HGB", "Stage", "Age", "Drug", "Chemo_before_IO", "HLA_LOH", "MSI", "Sex"]
 
 x_train = pd.DataFrame(data, columns=rf16)
-rf_clf = RandomForestClassifier(random_state = 0, n_jobs = -1)
-grid_cv = GridSearchCV(rf_clf, param_grid = params, cv = 5, n_jobs = -1)
-grid_cv.fit(x_train, y_train.values.ravel())
+rfc = RandomForestClassifier(random_state = 0, n_jobs = -1)
+rfc_cv = GridSearchCV(rfc, param_grid = params, cv = 5, n_jobs = -1)
+rfc_cv.fit(x_train, y_train.values.ravel())
 
-print('Optimal Hyper Parameter, RF16: ', grid_cv.best_params_)
-print('Maximum Accuracy, RF16: {:.4f}'.format(grid_cv.best_score_))
+print('Optimal Hyper Parameter, RF16: ', rfc_cv.best_params_)
+print('Maximum Accuracy, RF16: {:.4f}'.format(rfc_cv.best_score_))
 
 ## GridSearchCV for RF11
 rf11=["HED", "TMB", "FCNA", "BMI", "NLR", "Stage", "Age", "Drug", "HLA_LOH", "MSI", "Sex"]
 
 x_train = pd.DataFrame(data, columns=rf11)
-rf_clf = RandomForestClassifier(random_state = 0, n_jobs = -1)
-grid_cv = GridSearchCV(rf_clf, param_grid = params, cv = 5, n_jobs = -1)
-grid_cv.fit(x_train, y_train.values.ravel())
+rfc = RandomForestClassifier(random_state = 0, n_jobs = -1)
+rfc_cv = GridSearchCV(rfc, param_grid = params, cv = 5, n_jobs = -1)
+rfc_cv.fit(x_train, y_train.values.ravel())
 
-print('Optimal Hyper Parameter, RF11: ', grid_cv.best_params_)
-print('Maximum Accuracy, RF11: {:.4f}'.format(grid_cv.best_score_))
+print('Optimal Hyper Parameter, RF11: ', rfc_cv.best_params_)
+print('Maximum Accuracy, RF11: {:.4f}'.format(rfc_cv.best_score_))

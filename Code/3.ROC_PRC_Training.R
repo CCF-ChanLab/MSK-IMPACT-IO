f389c26fdc2404c9ca765fc416a970a54527a404
CCF-ChanLab <72822072+CCF-ChanLab@users.noreply.github.com>
2020-12-10 20:16:19 -0500

    Update 3.ROC_PRC_Training.R

--- Code/3.ROC_PRC_Training.R
+++ Code/3.ROC_PRC_Training.R
@@ -4,7 +4,7 @@ library(pROC)
 
 data <- read.table('Training_RF_Prob.txt', header=T, sep='\t')
 ## Generate ROC and PRC, Pan-cancer
-scores1 <- join_scores(data$RF16_prob, data$RF11_prob, data$TMB_score)
+scores1 <- join_scores(data$RF16_prob, data$RF11_prob, data$TMB)
 msmdat <- mmdata(scores1, data$Response)
 msmdat2 <- mmdata(scores1, data$Response, modnames = c("RF16", "RF11", "TMB"))
 mscurves <- evalmod(msmdat2)
@@ -21,7 +21,7 @@ legend("bottomright", legend=c("RF16"), col=c("red"), lwd=2)
 
 ## Generate ROC and PRC, Melanoma
 data2 <- data[grep("0", data$Cancer_Type),]
-scores1 <- join_scores(data2$RF16_prob,data2$RF11_prob,data2$TMB)
+scores1 <- join_scores(data2$RF16_prob, data2$RF11_prob, data2$TMB)
 msmdat <- mmdata(scores1, data2$Response)
 msmdat2 <- mmdata(scores1, data2$Response, modnames = c("RF16","RF11","TMB"))
 mscurves <- evalmod(msmdat2)
@@ -38,7 +38,7 @@ legend("bottomright", legend=c("RF16"), col=c("red"), lwd=2)
 
 ## Generate ROC and PRC, NSCLC
 data2 <- data[grep("1", data$Cancer_Type),]
-scores1 <- join_scores(data2$RF16_prob,data2$RF11_prob,data2$TMB)
+scores1 <- join_scores(data2$RF16_prob, data2$RF11_prob, data2$TMB)
 msmdat <- mmdata(scores1, data2$Response)
 msmdat2 <- mmdata(scores1, data2$Response, modnames = c("RF16","RF11","TMB"))
 mscurves <- evalmod(msmdat2)
@@ -55,7 +55,7 @@ legend("bottomright", legend=c("RF16"), col=c("red"), lwd=2)
 
 ## Generate ROC and PRC, Others
 data2 <- data[grep("2", data$Cancer_Type),]
-scores1 <- join_scores(data2$RF16_prob,data2$RF11_prob,data2$TMB)
+scores1 <- join_scores(data2$RF16_prob, data2$RF11_prob, data2$TMB)
 msmdat <- mmdata(scores1, data2$Response)
 msmdat2 <- mmdata(scores1, data2$Response, modnames = c("RF16","RF11","TMB"))
 mscurves <- evalmod(msmdat2)
